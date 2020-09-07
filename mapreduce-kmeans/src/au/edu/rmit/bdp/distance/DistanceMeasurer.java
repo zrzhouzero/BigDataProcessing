70a06378b1d3341c5f8371b53c97fee300b1b21d
Zéro <38496997+zrzhouzero@users.noreply.github.com>
2020-09-07 12:55:07 +0800

    Add files via upload

---
+++ mapreduce-kmeans/src/au/edu/rmit/bdp/distance/DistanceMeasurer.java
@@ -0,0 +1,11 @@
+package au.edu.rmit.bdp.distance;
+
+import de.jungblut.math.DoubleVector;
+
+public interface DistanceMeasurer {
+
+	public double measureDistance(double[] set1, double[] set2);
+
+	public double measureDistance(DoubleVector vec1, DoubleVector vec2);
+
+}
